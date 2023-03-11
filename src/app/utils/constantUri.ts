23bb4f00b68aded15396ab2215b485a7d28d6b75
a3521110330@alumno.uttehuacan.edu.mx <a3521110330@alumno.uttehuacan.edu.mx>
2023-03-11 16:01:11 -0600

    first commit

---
+++ src/app/utils/constantUri.ts
@@ -0,0 +1,6 @@
+const baseUrl = 'https://api.themoviedb.org/21';
+export class constantUri {
+    public static readonly apikey = '778694ebf1e21537e00ad0102f26b712';
+    public static readonly validateWithLogin = baseUrl+ '/authentication/token/validate_with_login';
+    public static readonly tokenNew = baseUrl+ '/authentication/token/new';
+}
